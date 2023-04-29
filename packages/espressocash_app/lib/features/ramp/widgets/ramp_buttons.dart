29ece64d6191ab10731b5b9c159635f195b7b316
Justin Enerio <justinenerio@pm.me>
2023-04-29 14:58:26 +0800

    initial upd

--- packages/espressocash_app/lib/features/ramp/widgets/ramp_buttons.dart
+++ packages/espressocash_app/lib/features/ramp/widgets/ramp_buttons.dart
@@ -1,6 +1,6 @@
-import 'package:dfunc/dfunc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ramp_flutter/configuration.dart';
 import 'package:ramp_flutter/ramp_flutter.dart';
 
 import '../../../../../l10n/l10n.dart';
@@ -8,7 +8,6 @@ import '../../../config.dart';
 import '../../../core/accounts/bl/account.dart';
 import '../../../core/balances/context_ext.dart';
 import '../../../ui/button.dart';
-import '../src/widgets/off_ramp_bottom_sheet.dart';
 
 class AddCashButton extends StatelessWidget {
   const AddCashButton({
@@ -26,15 +25,15 @@ class AddCashButton extends StatelessWidget {
           text: context.l10n.addCash,
           onPressed: () {
             final configuration = _defaultConfiguration
+              ..defaultFlow = 'ONRAMP'
               ..userAddress =
                   context.read<MyAccount>().wallet.publicKey.toBase58();
 
-            RampFlutter.showRamp(
-              configuration,
-              (_, __, ___) {},
-              () => context.notifyBalanceAffected(),
-              ignore,
-            );
+            RampFlutter()
+              ..onRampClosed = () {
+                context.notifyBalanceAffected();
+              }
+              ..showRamp(configuration);
           },
         ),
       );
@@ -54,15 +53,32 @@ class CashOutButton extends StatelessWidget {
           size: size,
           minWidth: 250,
           text: context.l10n.cashOut,
-          onPressed: () => OffRampBottomSheet.show(context),
+          onPressed: () {
+            final configuration = _defaultConfiguration
+              ..defaultFlow = 'OFFRAMP'
+              ..useSendCryptoCallback = true
+              ..userAddress =
+                  context.read<MyAccount>().wallet.publicKey.toBase58();
+
+            final ramp = RampFlutter()
+              ..onRampClosed = () {
+                context.notifyBalanceAffected();
+              }
+              ..onSendCryptoRequested = (payload) {
+                print('offramp created: $payload');
+              }
+              ..showRamp(configuration);
+
+            // ramp.sendCrypto('todo');
+          },
         ),
       );
 }
 
 final _defaultConfiguration = Configuration()
+  ..hostApiKey = rampApiKey
   ..hostAppName = 'Espresso Cash'
   ..hostLogoUrl =
       'https://www.espressocash.com/landing/img/asset-2-2x-copy@2x.png'
-  ..hostApiKey = rampApiKey
   ..swapAsset = 'SOLANA_USDC'
   ..defaultAsset = 'SOLANA_USDC';
